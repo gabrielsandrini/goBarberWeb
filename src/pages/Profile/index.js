8ac59fb998c2c62bfbbb1c6edb0d9c51a2a4a6a2
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-04-08 00:51:58 -0300

    update profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+        <Input name="password" type="password" placeholder="Sua senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
